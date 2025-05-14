a857d1825f383c2555191c69e29de2b40ee03fa2
Isac Kim <isac08270@gmail.com>
2025-05-14 21:13:29 +1000

    changed app theme colors

--- src/Hitorus.Web/Layout/MainLayout.razor.cs
+++ src/Hitorus.Web/Layout/MainLayout.razor.cs
@@ -12,8 +12,26 @@ namespace Hitorus.Web.Layout {
         [Inject] private IConfiguration HostConfiguration { get; set; } = default!;
         [Inject] IStringLocalizer<SharedResource> SharedLocalizer { get; set; } = default!;
 
+        // Helper websites
+        // https://coolors.co/palettes/popular
+        // https://m2.material.io/design/color/the-color-system.html#tools-for-picking-colors
+        private const string COLOR_PRIMARY_LIGHT = "#ed6a5a";
+        private const string COLOR_PRIMARY_DARK = "#e3847c";
         private MudThemeProvider _mudThemeProvider = null!;
-        private readonly MudTheme _theme = new();
+        private readonly MudTheme _theme = new() {
+            PaletteLight = new() {
+                Primary = COLOR_PRIMARY_LIGHT,
+                Secondary = "#770a00",
+                Tertiary = "#5c005f",
+                AppbarBackground = COLOR_PRIMARY_LIGHT
+            },
+            PaletteDark = new() {
+                Primary = COLOR_PRIMARY_DARK,
+                Secondary = "#a32116",
+                Tertiary = "#960f7b",
+                AppbarBackground = COLOR_PRIMARY_DARK
+            }
+        };
         private bool _isDarkMode;
         private bool _drawerOpen = true;
 

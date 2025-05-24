1e6fb8c2ed352067f2738f0251e340fc0abd9003
Isac Kim <isac08270@gmail.com>
2025-05-24 23:30:29 +1000

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Generate localization resource files
-python generate-resx.py
+# Generate Hitorus.Web localization resource files
+python generate-resx.py 2
 # Build Hiotrus.Web
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
-./dotnet/dotnet --version
+./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet publish src/Hitorus.Web -c Release -o output

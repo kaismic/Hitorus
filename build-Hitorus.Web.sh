b9be4826f978cf4a135df722283ecaade944bc8f
Isac Kim <isac08270@gmail.com>
2025-06-03 01:08:33 +1000

    attempt fix script

--- build-Hitorus.Web.sh
+++ build-Hitorus.Web.sh
@@ -6,7 +6,7 @@ python generate-resx.py 2
 curl -sSL https://dot.net/v1/dotnet-install.sh > dotnet-install.sh
 chmod +x dotnet-install.sh
 ./dotnet-install.sh -c 9.0 -InstallDir ./dotnet
-./dotnet/dotnet workload install wasm-tools
+# ./dotnet/dotnet workload install wasm-tools
 ./dotnet/dotnet publish src/Hitorus.Web -c Release -o output
-find . -name "*.br" -type f -delete
-find . -name "*.gz" -type f -delete
+# find ./output/wwwroot -name "*.br" -type f -delete
+# find ./output/wwwroot -name "*.gz" -type f -delete

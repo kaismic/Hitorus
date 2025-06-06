2a8bc3fb65110b3b074f47219993f02a595b6068
Isac Kim <isac08270@gmail.com>
2025-06-06 22:52:52 +1000

    added thumbnail image count key

--- src/Hitorus.Web/LocalStorageKeys.cs
+++ src/Hitorus.Web/LocalStorageKeys.cs
@@ -1,5 +1,6 @@
 ﻿namespace Hitorus.Web {
     public class LocalStorageKeys {
         public const string API_PORT = "Hitorus.apiPort";
+        public const string THUMBNAMIL_IMAGE_COUNT = "Hitorus.thumbnailImageCount";
     }
 }
